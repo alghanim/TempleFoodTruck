adb1507c773752262d09a518cfca809ffb32e293
Rajinder Kumar <rajinder.kumar@temple.edu>
2014-11-15 13:47:55 -0600

    Added Code for finding user's current location. (not working yet, gives an error)

--- TempleFoodTruck/src/com/example/templefoodtruck/MainActivity.java
+++ TempleFoodTruck/src/com/example/templefoodtruck/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.templefoodtruck;
 
-import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends FragmentActivity{
+
+	private GoogleMap googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // first commit
-        // testing
+
+        googleMap = (SupportMapManager) getSupportFragmentManager().findFragmentById(R.id.map).getMap();
+        UserCurrentLocation();
     }
+    
+	private void UserCurrentLocation() {
+		googleMap.setMyLocationEnabled(true);
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		String provider = locationManager.getBestProvider(criteria, true);
+		Location myLocation = locationManager.getLastKnownLocation(provider);
+		googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		double latitude = myLocation.getLatitude();
+		double longitude = myLocation.getLongitude();
+		LatLng latLng = new LatLng(latitude, longitude);
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+		googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+		
+		
+	}
+
+
 }
