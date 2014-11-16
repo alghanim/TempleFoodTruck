7ba421be1b1deaa4b84f1dc5a9d7cf37d5833abf
Rajinder Kumar <rajinder.kumar@temple.edu>
2014-11-15 23:40:02 -0600

    Fixed zoom in for user's current location

--- TempleFoodTruck/src/com/example/templefoodtruck/MainActivity.java
+++ TempleFoodTruck/src/com/example/templefoodtruck/MainActivity.java
@@ -4,13 +4,11 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,8 +26,8 @@ public class MainActivity extends FragmentActivity{
     }
 
 	private void getMap() {
-		android.app.FragmentManager fm = getFragmentManager();
-		MapFragment fragment = (MapFragment) fm.findFragmentById(R.id.map);
+		FragmentManager fm = getSupportFragmentManager();
+		SupportMapFragment fragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
 		googleMap = fragment.getMap();      	
 	}
 
@@ -43,9 +41,9 @@ public class MainActivity extends FragmentActivity{
 		double latitude = myLocation.getLatitude();
 		double longitude = myLocation.getLongitude();
 		LatLng latLng = new LatLng(latitude, longitude);
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-		googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
-		googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));	
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));		
+		googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
 	}
 
 
